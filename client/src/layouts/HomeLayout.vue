6d63b292f68db53c9b667ccbd1b29f917fb99187
boaloysius <boaloysius@gmail.com>
2024-08-25 01:02:59 +0530

    image optimizations

---
+++ client/src/layouts/HomeLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <CloudinaryImage
+    publicId="heavenly-heights/core/bg"
+    :attrs="{
+      class: 'absolute inset-0 w-full object-cover object-top h-full',
+      alt: 'Mountains and forests with two cabins',
+    }"
+  />
+  <router-view />
+</template>
+<script setup>
+import { CloudinaryImage } from "@/components/cloudinary-image";
+import { RouterView } from "vue-router";
+</script>
